669ff3de276fa37c50780bfab7384ed7d7e72a49
Thomasomoke <omokethomas644@gmail.com>
2024-10-17 14:24:11 +0000

    table with unique attributes

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
---SQL script that create a table users
---with id,emaIl and name as attributes
+-- SQL script that create a table users
+-- with id,emaIl and name as attributes
 CREATE TABLE IF NOT EXISTS users (
+	-- create a table if it does not exist
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
