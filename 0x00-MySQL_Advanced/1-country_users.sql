2db8a2df3e99df56b6f8fc8308482d1734296fcd
Thomasomoke <omokethomas644@gmail.com>
2024-10-17 14:38:36 +0000

    added an attribute that store predefined values

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,10 +1,9 @@
 -- SQL script that create a table users
--- with id,emaIl and name as attributes
+-- with id,email and name as attributes
 CREATE TABLE IF NOT EXISTS users (
 	-- create a table if it does not exist
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
-	-- added user's country cannot be null
+	name VARCHAR(255),
 	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
