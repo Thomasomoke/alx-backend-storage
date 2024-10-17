820b84cb7496fba4f61ee1df27a8282f76934529
Thomasomoke <omokethomas644@gmail.com>
2024-10-17 18:38:45 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,16 +1,16 @@
---  a SQL script that lists all bands with Glam rock
--- as their main style, ranked by their longevity
+-- A SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+-- Calculating lifespan in years, until 2022 (or the split year, if applicable)
 
 SELECT
-	band_name,
-	CASE
-		-- If the band split, calculate lifespan using the split year
-		WHEN split IS NOT NULL THEN (split - formed)
-		ELSE (2022 - formed)
-	END AS lifespan
+    band_name,
+    CASE
+        -- If the band has split, calculate lifespan using the split year
+        WHEN split IS NOT NULL THEN (split - formed)
+        ELSE (2022 - formed)
+    END AS lifespan
 FROM
-	metal_bands
+    metal_bands
 WHERE
-	style = 'Glam rock'
+    style = 'Glam rock'
 ORDER BY
-	lifespan DESC;
+    lifespan DESC;
