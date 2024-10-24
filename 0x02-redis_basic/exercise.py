45ac8fc5a96d5b1efc32cb409c9308a339918da3
Thomasomoke <omokethomas644@gmail.com>
2024-10-24 08:10:11 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
-"""
-cache class in __init__ method,store an instance
-of Redis client and flush the instance
-"""
 import redis
 import uuid
-from typing import Union
-"""importing relevant
-modules
-"""
+from typing import Union, Callable, Optional
 
 
 class Cache:
     def __init__(self):
         """Initialize the Cache instance with
-        a redis client and flush db
+        a Redis client and flush the database
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """store data in Redis with a randomly generated key"""
+        """Store data in Redis with a randomly generated key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            str, bytes, int, float, None]:
+        """Retrieve data from Redis and apply the
+        conversion function if provided"""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve data as a UTF-8 decoded string"""
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve data as an integer"""
+        return self.get(key, fn=int)
